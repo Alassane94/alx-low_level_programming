753e4694061a4440ea3f078af87265820337382c
Alassane Yeo <Yeoalassane94@gmail.com>
2022-09-19 09:53:50 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 
 /**
 
@@ -16,9 +14,8 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
 }
-
