085b4553e2bb8d1862f20460e09aa56683aab21b
Alassane Yeo <Yeoalassane94@gmail.com>
2022-09-20 02:54:32 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
+ * main - print a string
+ * Return: always (0)
  */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
