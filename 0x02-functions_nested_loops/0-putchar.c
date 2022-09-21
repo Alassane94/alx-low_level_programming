389ea382383066858ca4e21ecac771314daa53d4
Alassane Yeo <Yeoalassane94@gmail.com>
2022-09-21 00:53:56 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
 #include "holberton.h"
 
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
 
 	char *sh = "Holberton";
-
 
 
 	while (*sh)
@@ -30,7 +24,6 @@ int main(void)
 
 	_putchar('\n');
 
-
-
 	return (0);
+}
 
