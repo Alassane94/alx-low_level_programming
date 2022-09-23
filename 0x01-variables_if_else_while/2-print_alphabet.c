6e40ab0ce6d63cb78f72810cbb2273860b1ac8c1
Alassane Yeo <Yeoalassane94@gmail.com>
2022-09-23 10:14:06 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,73 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
-
- * main - Prints numbers between 00 to 89.
-
+ * main - Prints the alphabet.
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	int i, e;
-
-
-
-	i = 48;
-
-	e = 48;
-
-
-
-	while (e < 58)
-
+	for (i = 0; i < 26; i++)
 	{
-
-		i = 48;
-
-		while (i < 58)
-
-		{
-
-			if (e != i && e < i)
-
-			{
-
-				putchar(e);
-
-				putchar(i);
-
-				if (i == 57 && e == 56)
-
-				{
-
-					break;
-
-				}
-
-				putchar(',');
-
-				putchar(' ');
-
-			}
-
-			i++;
-
-		}
-
-		e++;
-
+		putchar(alp[i]);
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
